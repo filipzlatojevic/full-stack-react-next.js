3df84db6ba70990e1971bd0bf648521b27d82b80
filipzlatojevic <ficozlatojevic@gmail.com>
2023-06-02 20:36:59 +0200

    feat(mongo db): create mongo db...

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -17,6 +17,7 @@ const getData = (cat) => {
 
 const Category = ({ params }) => {
   const data = getData(params.category);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
@@ -29,12 +30,7 @@ const Category = ({ params }) => {
             <Button text="See More" url="#" />
           </div>
           <div className={styles.imgContainer}>
-            <Image
-              className={styles.img}
-              fill={true}
-              src={item.image}
-              alt=""
-            />
+            <Image className={styles.img} fill={true} src={item.image} alt="" />
           </div>
         </div>
       ))}
