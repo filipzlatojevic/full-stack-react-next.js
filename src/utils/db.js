ce7ae36b94381892270b237cccfe191bc1f3986d
filipzlatojevic <ficozlatojevic@gmail.com>
2023-06-16 22:03:47 +0200

    fix

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
 const connect = async () => {
+  mongoose.set("strictQuery", true);
+
   try {
     await mongoose.connect(process.env.MONGO);
   } catch (error) {
@@ -9,3 +11,26 @@ const connect = async () => {
 };
 
 export default connect;
+
+// let isConnected = false;
+
+// export const connect = async () => {
+//   mongoose.set("strictQuery", true);
+
+//   if (isConnected) {
+//     console.log("Already connected to the database.");
+//     return;
+//   }
+
+//   try {
+//     await mongoose.connect(process.env.MONGO, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     isConnected = true;
+//     console.log("MongoDB connected successfully.");
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error);
+//     isConnected = false;
+//   }
+// };
